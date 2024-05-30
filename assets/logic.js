a2e67e5b07e43bea4587154c43b53dfd40363c30
khaled El-henaweey <71648492+khaled-elhenaweey@users.noreply.github.com>
2024-05-30 14:40:57 +0300

    use fetch

--- assets/logic.js
+++ assets/logic.js
@@ -1,13 +1,12 @@
-// get api user request:
-function getAllPosts() {
-  let request = new XMLHttpRequest();
-  request.open("GET", "https://jsonplaceholder.typicode.com/users");
-  request.responseType = "json";
-  request.send();
-  request.onload = function () {
-    // check the request status error
-    if (request.status >= 200 && request.status < 300) {
-      let users = request.response;
+function getAllUsers() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((users) => {
       // loop on the api obj and build the UI
       for (const user of users) {
         let index = user.id;
@@ -19,28 +18,24 @@ function getAllPosts() {
         `;
         document.querySelector("#actionCard").innerHTML += contentAction;
       }
-    } else {
-      alert(request.status);
-    }
-  };
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
 }
-// filter api post request:
 
-function FilterPost(index) {
-  let request = new XMLHttpRequest();
-  // get the posts form post api and set the user index in the url
-  request.open(
-    "GET",
-    `https://jsonplaceholder.typicode.com/posts?userId=${index}`
-  );
-  request.responseType = "json";
-  request.send();
-  request.onload = function () {
-    // check the request status error
-    if (request.status >= 200 && request.status < 300) {
+//active links and call the filter posts
+function userPosts(index) {
+  fetch(`https://jsonplaceholder.typicode.com/posts?userId=${index}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((posts) => {
       // clear the UI Posts
       document.querySelector("#postList").innerHTML = "";
-      let posts = request.response;
       // loop on the api obj and build the UI
       for (const post of posts) {
         let contentAction = `
@@ -51,14 +46,10 @@ function FilterPost(index) {
           `;
         document.querySelector("#postList").innerHTML += contentAction;
       }
-    } else {
-      alert(request.status);
-    }
-  };
-}
-//active links and call the filter posts
-function userPosts(index) {
-  FilterPost(index);
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
   const links = document.querySelectorAll(".user-links li");
   // Remove active class from all links
   links.forEach((l) => l.classList.remove("active"));
@@ -66,4 +57,4 @@ function userPosts(index) {
   links[index - 1].classList.add("active");
 }
 // call the get api
-getAllPosts();
+getAllUsers();
